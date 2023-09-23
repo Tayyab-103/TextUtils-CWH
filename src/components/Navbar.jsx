ba480b4360145f833cf62e6bafab3e5c88380929
Tayyab-103 <tayyabhameed103@gmail.com>
2023-09-23 12:42:09 -0700

    navbarr fixed top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar(props) {
   return (
     <>
       <nav
-        className={`navbar navbar-expand-lg navbar-${props.mode}  bg-${props.mode} `}
+        className={`navbar navbar-expand-lg navbar-${props.mode}  bg-${props.mode} fixed-top`}
       >
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
